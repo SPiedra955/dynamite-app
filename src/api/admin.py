fbee18c40fbbb552ee5340fe195e62a4bfe6adff
Samuel Piedra <114516225+SPiedra955@users.noreply.github.com>
2026-05-08 14:18:03 +0000

    starting models and data migration

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,17 +1,34 @@
-  
+
+from flask_admin.contrib.sqla import ModelView
 import os
 from flask_admin import Admin
-from .models import db, User
-from flask_admin.contrib.sqla import ModelView
+from .models import db,
+User,
+Product,
+Order,
+OrderItem,
+SubscriptionPlan,
+Subscription,
+Payment,
+Cart,
+CartItem
+
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
-    
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Product, db.session))
+    admin.add_view(ModelView(Order, db.session))
+    admin.add_view(ModelView(OrderItem, db.session))
+    admin.add_view(ModelView(SubscriptionPlan, db.session))
+    admin.add_view(ModelView(Subscription, db.session))
+    admin.add_view(ModelView(Payment, db.session))
+    admin.add_view(ModelView(Cart, db.session))
+    admin.add_view(ModelView(CartItem, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
