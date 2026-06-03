87eb73a4ef1396eb6c24b4b73d9179445e7fbffa
Samuel Piedra <114516225+SPiedra955@users.noreply.github.com>
2026-06-03 09:31:33 +0000

    fixing blueprint imports

--- src/api/blueprint.py
+++ src/api/blueprint.py
@@ -3,12 +3,12 @@ from flask import Blueprint # type: ignore
 api = Blueprint('api', __name__)
 
 
-from api.api_routes.subscription_plans import *
-from api.api_routes.myplans import *
-from api.api_routes.payment import *
-from api.api_routes.payments import *
-from api.api_routes.products import *
-from api.api_routes.carts import *
-from api.api_routes.orders import *
-from api.api_routes.subscription import *
-from api.api_routes.users import *
+from src.api.api_routes.subscription_plans import *
+from src.api.api_routes.myplans import *
+from src.api.api_routes.payment import *
+from src.api.api_routes.payments import *
+from src.api.api_routes.products import *
+from src.api.api_routes.carts import *
+from src.api.api_routes.orders import *
+from src.api.api_routes.subscription import *
+from src.api.api_routes.users import *
